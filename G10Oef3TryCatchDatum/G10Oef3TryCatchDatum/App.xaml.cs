56d658630003998f40ca2ce5d85099d4d6157bd0
Indy Iri <indy.iri@student.pxl.be>
2024-11-14 11:07:04 +0100

    wpf aangemaakt

---
+++ G10Oef3TryCatchDatum/G10Oef3TryCatchDatum/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace G10Oef3TryCatchDatum
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
